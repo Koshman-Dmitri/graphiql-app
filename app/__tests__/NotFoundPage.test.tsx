1acf6724b276fcc209ad9d7714072ec58ba7fcc0
Koshman-Dmitri <123456desher@rambler.ru>
2024-08-31 10:06:03 +0300

    feat: page tests

--- app/__tests__/NotFoundPage.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import NotFoundPage from '../[locale]/[...not-found]/page';
-
-describe('NotFoundPage', () => {
-  test('Should render without crashing', async () => {
-    render(await NotFoundPage({ params: { slug: [], locale: 'en' } }));
-  });
-});
---
+++ app/__tests__/Pages.test.tsx
@@ -0,0 +1,32 @@
+import { render } from '@testing-library/react';
+import NotFoundPage from '../[locale]/[...not-found]/page';
+import GraphQLPage from '../[locale]/graphql/[[...slug]]/page';
+import HistoryPage from '../[locale]/history/page';
+import MainPage from '../[locale]/main/page';
+import RestPage from '../[locale]/rest/[[...slug]]/page';
+
+describe('Should render without crashing', () => {
+  beforeEach(() => {
+    vi.spyOn(console, 'error').mockImplementation(() => null);
+  });
+
+  test('Not Found Page', async () => {
+    render(await NotFoundPage({ params: { slug: [], locale: 'en' } }));
+  });
+
+  test('GraphQl Page', async () => {
+    render(await GraphQLPage({ params: { slug: [], locale: 'en' }, searchParams: {} }));
+  });
+
+  test('History Page', async () => {
+    render(await HistoryPage({ params: { slug: [], locale: 'en' }, searchParams: {} }));
+  });
+
+  test('Main Page', async () => {
+    render(await MainPage({ params: { slug: [], locale: 'en' } }));
+  });
+
+  test('Rest Page', async () => {
+    render(await RestPage({ params: { slug: [], locale: 'en' }, searchParams: {} }));
+  });
+});
