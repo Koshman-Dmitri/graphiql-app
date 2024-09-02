7fbb48f09002e4c740868198e5cce147d95083a8
Koshman-Dmitri <112682930+Koshman-Dmitri@users.noreply.github.com>
2024-09-02 20:24:19 +0300

    Rest/graphql/history tests (#6)
    * feat: add util function tests
    
    * feat: add graphql form tests
    
    * feat: add rest components tests
    
    * fix: tests for i18n
    
    * fix: i18n warn
    
    * feat: page tests
    
    * fix: typescript version
    
    * feat: language tests

---
+++ app/__tests__/setup.ts
@@ -0,0 +1,34 @@
+import { cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/vitest';
+
+afterEach(() => {
+  cleanup();
+});
+
+vitest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      route: '/',
+      pathname: '/',
+      query: '',
+      asPath: '',
+      locale: 'no',
+      push: vi.fn(),
+      reload: vi.fn(),
+      refresh: vi.fn(),
+      events: {
+        on: vi.fn(),
+        off: vi.fn(),
+      },
+      beforePopState: vi.fn(() => null),
+      prefetch: vi.fn(() => null),
+    };
+  },
+  usePathname() {
+    return {
+      path: 'en/rest',
+      split: vi.fn().mockImplementation(() => ['foo', 'bar']),
+      replace: vi.fn(),
+    };
+  },
+}));
