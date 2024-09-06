4d510d6d112fb7cae0e5c5db9e6245915683a08c
Koshman-Dmitri <123456desher@rambler.ru>
2024-09-06 19:49:19 +0300

    fix: grapqhl folder name

--- app/__tests__/Pages.test.tsx
+++ app/__tests__/Pages.test.tsx
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import NotFoundPage from '../[locale]/[...not-found]/page';
-import GraphQLPage from '../[locale]/GRAPHL/[[...slug]]/page';
+import GraphQLPage from '../[locale]/GRAPHQL/[[...slug]]/page';
 import HistoryPage from '../[locale]/history/page';
 import RestPage from '../[locale]/[rest]/[...slug]/page';
 
