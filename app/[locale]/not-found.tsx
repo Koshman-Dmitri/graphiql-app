3ff8e747b12335e012451c2754fef7a9de468cdb
Koshman-Dmitri <123456desher@rambler.ru>
2024-09-10 12:02:06 +0300

    fix: not-found page props

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,6 +1,5 @@
-import { UrlParams } from '../utils/globalTypes';
 import NotFoundPage from './[...not-found]/page';
 
-export default function NotFound({ params }: UrlParams) {
-  return <NotFoundPage params={params} />;
+export default function NotFound() {
+  return <NotFoundPage />;
 }
